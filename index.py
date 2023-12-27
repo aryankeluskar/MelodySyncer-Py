972eaa15fbcf89d2333bb86cbe8ac5f25a78c60c
aryankeluskar <aryankeluskar6@gmail.com>
2023-12-26 20:28:40 -0700

    added CORS Origins

--- index.py
+++ index.py
@@ -13,6 +13,7 @@ from functools import partial
 import starlette
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 # import pymongo
 # re-push
@@ -23,6 +24,17 @@ load_dotenv()
 
 app = FastAPI()
 app.mount("/app", StaticFiles(directory="app"), name="app")
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 """
